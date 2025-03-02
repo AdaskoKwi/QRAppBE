bca240f4f21a46058dda1ae0d736d9299b3154d0
AdaskoKwi <adamnowakhomm@gmail.com>
2025-03-02 14:17:23 +0100

    Fixed lombok issues, minor tweaks.

--- src/main/java/org/example/qrappbe/controller/QRController.java
+++ src/main/java/org/example/qrappbe/controller/QRController.java
@@ -1,33 +1,41 @@
 package org.example.qrappbe.controller;
 
 import com.google.zxing.WriterException;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.example.qrappbe.service.QRService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.Base64;
 
 @RestController
 @RequestMapping("/api/qr")
+@RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:4200")
 public class QRController {
     private final QRService qrService;
 
-    public QRController(QRService qrService) {
-        this.qrService = qrService;
-    }
-
     @GetMapping("/get")
-    public String getQRCode(@RequestParam("text") String text) {
+    public ResponseEntity<QRresponse> getQRCode(@RequestParam("text") String text) {
         byte[] image;
 
         try {
-            image = qrService.getQRCode(text, 250,250);
+            image = qrService.getQRCode(text, 300,300);
+            return ResponseEntity
+                    .ok()
+                    .body(new QRresponse(image));
         } catch (IOException | WriterException e) {
             throw new RuntimeException(e);
         }
+    }
 
-        return Base64.getEncoder().encodeToString(image);
+    @Data
+    public static class QRresponse {
+        private byte[] qrCodeBase64;
+
+        public QRresponse(byte[] qrCodeBase64) {
+            this.qrCodeBase64 = qrCodeBase64;
+        }
     }
 }
--- src/main/java/org/example/qrappbe/service/QRService.java
+++ src/main/java/org/example/qrappbe/service/QRService.java
@@ -11,29 +11,16 @@ import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Service
+@NoArgsConstructor
 public class QRService {
-    public QRService() {
-    }
-
-    public void generateQRCode(String text, int width, int height, String filePath) throws WriterException, IOException {
-        QRCodeWriter qrCodeWriter = new QRCodeWriter();
-        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
-
-        Path path = FileSystems.getDefault().getPath(filePath);
-        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
-    }
-
     public byte[] getQRCode(String text, int width, int height) throws WriterException, IOException {
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
         BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
 
         ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
-        MatrixToImageConfig con = new MatrixToImageConfig( 0xFF000002 , 0xFFFFC041 );
+        MatrixToImageConfig con = new MatrixToImageConfig( 0xFF000002 , 0x000000 );
 
         MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream, con);
 
